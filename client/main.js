930d137bc9034567345c3bfd21fa9b1231444e6f
Mark Willisford <MarkWillisford@users.noreply.github.com>
2020-07-16 20:25:28 -0700

    bug squishy and simple ui design

--- client/main.js
+++ client/main.js
@@ -1,4 +1,16 @@
 import { Template } from 'meteor/templating';
+import { Notes } from '../lib/collections';
 
 import './main.html';
 
+Template.body.helpers({
+/*   notes:[
+    {text:'My Note 1'},
+    {text:'My Note 2'},
+    {text:'My Note 3'},
+    {text:'My Note 4'}
+  ] */
+  notes(){
+    return Notes.find({});
+  }
+})
--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
+import { Notes } from '../lib/collections';
 
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+// export const Notes = new Mongo.Collection('notes');
