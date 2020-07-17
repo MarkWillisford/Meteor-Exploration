d8b16851ca4ea8bd846463679c8762d890e3efaa
Mark Willisford <MarkWillisford@users.noreply.github.com>
2020-07-16 22:00:56 -0700

    remove task

--- client/main.js
+++ client/main.js
@@ -13,4 +13,36 @@ Template.body.helpers({
   notes(){
     return Notes.find({});
   }
-})
+});
+
+Template.add.events({
+  'submit .add-form': function(){
+    event.preventDefault();
+
+    // Get input value
+    const target = event.target;
+    const text = target.text.value;
+
+    // INsert node into collection
+    Notes.insert({
+      text,
+      createdAt: new Date()
+    });
+
+    // clear form
+    target.text.value = '';
+
+    // Close modal
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+    
+    return false;
+  }
+});
+
+Template.note.events({
+  'click .delete-note': function(){
+    Notes.remove(this._id);
+    return false;
+  }
+});
